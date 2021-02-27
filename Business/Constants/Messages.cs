f442c05a5c85f2a3f94cd5d19f4ca9126b793722
OzyyDemir <oguzhandmr98@gmail.com>
2021-02-27 18:22:13 +0300

    24 şubat commit

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,8 +8,17 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün Eklendi";
+
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
+
         public static string MaintenanceTime="Sistem bakımda";
+
         public static string ProductsListed="Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Fazla ürün oldu ekleyemez artık";
+
+        public static string ProductNameAlreadyExists = "Bu isimli üründen zaten listede var";
+
+        public static string CategoryLimitExceded = "Kategori limiti aşıldı";
     }
 }
--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -12,6 +12,8 @@ namespace Core.Aspects.Autofac.Validation
     public class ValidationAspect : MethodInterception
     {
         private Type _validatorType;
+
+
         public ValidationAspect(Type validatorType)
         {
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
